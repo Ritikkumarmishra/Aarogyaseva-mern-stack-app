69ccb33fedc800c53e9b99217e608608a1194559
Ritik Kumar Mishra <ritiknarhan8051@gmail.com>
2026-02-11 19:46:27 +0530

    Initial commit

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      I am your about page
+    </div>
+  )
+}
+
+export default About
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      You are at home page
+    </div>
+  )
+}
+
+export default Home
